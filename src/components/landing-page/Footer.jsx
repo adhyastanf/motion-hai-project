e7b849aac04a5f57012a4dd11914ef60ce864fd8
Rafa <rafamaheswaraxd@gmail.com>
2025-05-26 22:19:14 +0700

    ketinggalan footer nya bray

--- src/components/landing-page/Footer.jsx
+++ src/components/landing-page/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
                         <span className="text-xl font-bold">Hai Motion</span>
                     </div>
                     <p className="text-white/70 leading-relaxed">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Kami bukan hanya sekadar penyedia jasa kami adalah partner kreatif yang siap tumbuh bersama Anda. Setiap proyek kami kerjakan dengan penuh riset, ide segar, dan sentuhan profesional, demi hasil yang tidak hanya indah dipandang, tapi juga berdampak nyata.
                     </p>
                 </div>
 
