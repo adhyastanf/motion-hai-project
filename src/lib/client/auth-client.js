11fc0f0b2a3ea6de246784fc092b460d438ea451
adhyastanf <adhyasta.nf@gmail.com>
2025-05-27 11:06:19 +0700

    fix base url

--- src/lib/client/auth-client.js
+++ src/lib/client/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from 'better-auth/client'; // make sure to import fr
 import { organizationClient } from 'better-auth/client/plugins';
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
   plugins: [organizationClient()],
 });
 
