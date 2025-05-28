e0b60b0313180ec4079ea667ae1057bea937db74
adhyastanf <adhyasta.nf@gmail.com>
2025-05-28 19:09:48 +0700

    fix build local

--- src/app/(landing-page)/layout.jsx
+++ src/app/(landing-page)/layout.jsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/landing-page/Navbar";
 import Footer from "@/components/landing-page/Footer";
 import Preloader from "@/components/landing-page/Preloader";
 import PreloaderProvider, { usePreloader } from "@/components/context/PreloaderContext";
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+// const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+// const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 function LandingContent({ children }) {
   const { showPreloader } = usePreloader();
 
   return (
-    <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <div className={`antialiased`}>
       {showPreloader ? (
         <Preloader />
       ) : (
