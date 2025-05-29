ee9f46025ac30768d77dd3b053ce2d135d6d7df6
adhyastanf <adhyasta.nf@gmail.com>
2025-05-29 20:56:48 +0700

    fix url string db

--- src/lib/db/drizzle.js
+++ src/lib/db/drizzle.js
@@ -1,3 +1,3 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 
-export const db = drizzle('mysql://root@localhost:3306/motion-hai')
+export const db = drizzle('mysql://haimotio_hai-motion:SW%40_%24c%29w_Wp%28@localhost:3306/haimotio_hai-motion');
