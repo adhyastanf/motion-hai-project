cf21c47d83d57e5a6db156c4d408945a99d096ac
adhyastanf <adhyasta.nf@gmail.com>
2025-05-29 18:48:25 +0700

    change version next 15

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
+
 export default defineConfig({
   dialect: 'mysql', // Menyebutkan MySQL sebagai database yang digunakan
   schema: 'src/lib/db/schema.ts', // Lokasi file skema tabel Anda
   out: 'src/lib/db/migrations-folder',
   dbCredentials: {
-    host: 'localhost',
-    user: 'root',
-    database: 'motion-hai',
+    host: 'brianza.id.rapidplex.com',
+    user: 'haimotio_hai-motion',
+    password: 'SW@_$c)w_Wp(',
+    database: 'haimotio_hai-motion',
   },
 });
