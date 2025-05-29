6ce5c29ba9736fd1ab10d2ad084e59e612ba0657
adhyastanf <adhyasta.nf@gmail.com>
2025-05-29 20:20:52 +0700

    set drizzle config

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,9 +5,10 @@ export default defineConfig({
   schema: 'src/lib/db/schema.ts', // Lokasi file skema tabel Anda
   out: 'src/lib/db/migrations-folder',
   dbCredentials: {
-    host: 'brianza.id.rapidplex.com',
+    host: 'localhost',
     user: 'haimotio_hai-motion',
     password: 'SW@_$c)w_Wp(',
     database: 'haimotio_hai-motion',
+    port: '3306',
   },
 });
